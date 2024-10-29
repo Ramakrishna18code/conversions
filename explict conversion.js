80aac931bf60762a438493086529afe39592df05
Rama krishna <rk4053727@gmail.com>
2024-10-29 16:16:00 +0530

    Create explict conversion.js

---
+++ explict conversion.js
@@ -0,0 +1,13 @@
+let result;
+
+
+result = Number("5");
+console.log(result, "-", typeof(result));
+
+
+result = String(true);
+console.log(result, "-", typeof(result));
+
+
+result = Boolean(0);
+console.log(result, "-", typeof(result));
